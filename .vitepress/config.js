f610e54cb6c53416260eb7497fc5c0017626191b
DanielSatzinger <satzingerdaniel12@gmail.com>
2024-06-26 11:46:03 +0200

    my third commit

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
-      { text: 'Home', link: '/' },
+      { text: 'Hommme', link: '/' },
       { text: 'Examples', link: '/markdown-examples' }
     ],
 
